4bcd79cad6021341790e79fbbf4c813054b20a2e
Subhro Chatterjee <subhro.techinfo@gmail.com>
2026-03-16 16:29:50 +0530

    allowdevorigin changes

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    allowedDevOrigins: ['local-origin.dev', '*.local-origin.dev','192.168.198.131'],
+};
 
 export default nextConfig;
